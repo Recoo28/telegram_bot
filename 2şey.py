2aadb22b58a97239fd460940c591d576868da03a
Recoo28 <61563638+Recoo28@users.noreply.github.com>
2024-07-26 01:48:53 +0300

    Add files via upload

--- 2şey.py
+++ 2şey.py
@@ -316,18 +316,6 @@ async def main():
         conn.close()
 
 if __name__ == "__main__":
-    import os
-    from flask import Flask
-
-    app = Flask(__name__)
-
-    @app.route('/')
-    def home():
-        return "Bot is running!"
-
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
